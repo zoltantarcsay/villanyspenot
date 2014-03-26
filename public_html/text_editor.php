143c3babc02e0d65d39539a9d9491ac391048a8d
simonzsolt <simonzsoltjota@gmail.com>
2014-03-26 19:15:01 +0100

    Update text_editor.php
    I'm using this editor: http://nicedit.com
    The "Elküld" button calls the script below to get the contetnt of the textarea, and also sends a hidden form, with the extracted text to the update_szoveg function at "Functions.php", which updates it in the database. There is a response whether the query is successful or not.

--- public_html/text_editor.php
+++ public_html/text_editor.php
@@ -1,18 +1,25 @@
-<head>
-</head>
-<body onLoad="Start()">
-<iframe onload="frameHeight();" id='edit' srcdoc='<?php echo htmlspecialchars($str_szoveg, ENT_QUOTES); ?>'></iframe>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<div >
+	<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+	<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>						
+	<textarea name="area" id="area" style="width: 100%;">
+		<?php echo htmlspecialchars($str_szoveg, ENT_QUOTES); ?>
+	</textarea>
+</div>
 <div id="menu_cont_bottom">
 	<div id="menu_left">
 			<form id="send" method="post">
 			<input type="hidden" name="text" value=""></input>
 			<input type="hidden" name="kod" value="<?php echo $kod; ?>"></input>
-			<input class="editor_btn" type="submit" value="Elküld" onclick="getText();"></input>
+			<input class="editor_btn" id="btn_send" type="submit" value="Elküld"></input>
 			</form>
 	</div>
-	<div id="menu_right">
-			<input type="checkbox" onclick="viewsource(this.checked)">View HTML Source</input>
-	</div>	
 </div>
-<script>
+<script type="text/javascript">
+	$("#btn_send").click(function () {
+		var nicInstance = nicEditors.findEditor('area');
+		var content = nicInstance.getContent();
+		var formInfo = document.forms['send'];
+		formInfo.text.value = content;
+	});
 </script>
